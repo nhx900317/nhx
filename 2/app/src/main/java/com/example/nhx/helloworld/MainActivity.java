c7627435623afb2a1333f73a3c7f5f5327225f18
nhx900317 <1018737497@qq.com>
2017-04-07 10:52:57 +0800

    add hello textview
    add hello textview

--- 2/app/src/main/java/com/example/nhx/helloworld/MainActivity.java
+++ 2/app/src/main/java/com/example/nhx/helloworld/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.nhx.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = (TextView)findViewById(R.id.hello);
+        tv1.setText("hello");
     }
 }
